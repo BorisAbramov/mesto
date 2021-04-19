d9f8bcb5cd27c773a8212bdcda9cc545ca1a3c02
Борис Абрамов <it.school136@gmail.com>
2021-04-19 22:31:06 +0300

    pr-6

--- scripts/validate.js
+++ scripts/validate.js
@@ -96,5 +96,9 @@ const showInputError = (inputElement, errorMessage) => {
   enableValidation({
     formSelector: ".popup__form",
     inputSelector: ".popup__input",
+    submitButtonSelector: '.popup__submit',
+    inactiveButtonClass: 'popup__submit_inactive',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
   });
   
